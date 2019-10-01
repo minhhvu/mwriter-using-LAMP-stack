679f52bfaf7800a226569f76cfda952c8e759166
minhh <minhhaivuhd@gmail.com>
2019-10-01 17:20:50 -0400

    add class to retrieve data from Google Books API

---
+++ app/Http/Controllers/Helper/GoogleBooksApi.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: minhh
+ * Date: 2019-10-01
+ * Time: 9:39 AM
+ */
+
+namespace App\Http\Controllers\Helper;
+
+class GoogleBook{
+    public $id, $title, $description, $coverLink, $previewLink, $publishDate, $authors, $textSnippet;
+}
+
+
+class GoogleBooksApi
+{
+    private $key = 'AIzaSyD4uYkk7VoLh0hxVfNxstg0wVtP3H9UiYw';
+    public $maxResults;
+    public $keywords;
+    private $query;
+    private $array;
+
+    public function __construct($keywords, $maxResults= 10){
+        $this->keywords = urlencode($keywords);
+        $this->maxResults = $maxResults;
+        $query = 'https://www.googleapis.com/books/v1/volumes?q='.$this->keywords.'&maxResults='.(string) $this->maxResults.'&key='.$this->key;
+        $this->query = $query;
+        $json = json_decode(file_get_contents($this->query), true);
+        $this->array = $json['items'];
+    }
+
+    //Return the api request and return the array of items
+    public function allBooks(){
+        $result = [];
+        foreach ($this->array as $item){
+            $x = new GoogleBook();
+            $x->id = $item['id'];
+            $x->title = $item['volumeInfo']['title'].', '.(isset($item['volumeInfo']['subtitle'])?$item['volumeInfo']['subtitle']:'');
+            $x->authors = isset($item['volumeInfo']['authors'])? $item['volumeInfo']['authors']: 'Unknown';
+            $x->publishDate = isset($item['volumeInfo']['publishedDate']) ? $item['volumeInfo']['publishedDate'] : 'Unknown';
+            $x->coverLink = isset($item['volumeInfo']['imageLink']['thumbnail'])?$item['volumeInfo']['imageLink']['thumbnail']: 'Not available';
+            $x->description = isset($item['volumeInfo']['description'])? $item['volumeInfo']['description']: '';
+            $x->previewLink = isset($item['volumeInfo']['previewLink'])? $item['volumeInfo']['previewLink']: 'Not available';
+            $x->textSnippet = isset($item['searchInfo']['textSnippet'])?$item['searchInfo']['textSnippet']:'';
+            $result[] = $x;
+        }
+        return $result;
+    }
+
+    public static function getBookAsString(GoogleBook $item){
+        return $item->title.'<br>'.'wrote by '.$item->authors[0].'<br>'.
+            $item->textSnippet.'<br><br>';
+    }
+
+}
